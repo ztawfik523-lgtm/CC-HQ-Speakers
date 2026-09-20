5300c1a651992c2093e36c1d911d6bb82551e702
ztawfik523-lgtm <324785736+ztawfik523-lgtm@users.noreply.github.com>
2026-09-20 23:54:26 +0300

    fix: import live stream group UUID

--- src/main/java/com/tom/hqspeaker/peripheral/HQSpeakerCompositePeripheral.java
+++ src/main/java/com/tom/hqspeaker/peripheral/HQSpeakerCompositePeripheral.java
@@ -29,6 +29,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
