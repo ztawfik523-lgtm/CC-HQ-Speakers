0be41e77a301d76ea363022ed23a028b26645da1
ztawfik523-lgtm <324785736+ztawfik523-lgtm@users.noreply.github.com>
2026-09-24 00:25:49 +0300

    feat: expose OpenAL source id for diagnostics

--- src/main/java/com/tom/hqspeaker/mixin/client/ChannelAccessor.java
+++ src/main/java/com/tom/hqspeaker/mixin/client/ChannelAccessor.java
@@ -2,10 +2,14 @@ package com.tom.hqspeaker.mixin.client;
 
 import com.mojang.blaze3d.audio.Channel;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Channel.class)
 public interface ChannelAccessor {
+    @Accessor("source")
+    int hqspeaker$getSource();
+
     @Invoker("pumpBuffers")
     void hqspeaker$pumpBuffers(int count);
 }
